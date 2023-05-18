ec6ad64bd8713ff64cfdd50efcc5b1ea434a3b62
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-05-18 15:45:16 -0700

    finished dark mode challenge

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,31 @@
-export default function DarkMode () {
+import React, { useState } from "react";
+export default function DarkMode() {
+  let [mode, setMode] = useState(true)
+
+  const handleClick = (e) => {
+    if (e.target.name === "light-mode") {
+      setMode(true)
+    } else {
+      setMode(false)
+    }
+  };
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={mode ? "page" : "page dark-mode"}>
+      <button
+        onClick={handleClick}
+        name="dark-mode"
+        className="dark-mode-button"
+      >
+        Dark Mode
+      </button>
+      <button
+        onClick={handleClick}
+        name="light-mode"
+        className="light-mode-button"
+      >
+        Light Mode
+      </button>
     </div>
-  )
+  );
 }
