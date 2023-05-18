4fdc8e8a729e5c286041c2476a390690e57c697f
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-05-18 14:27:22 -0700

    finished color renderer component

--- src/01-color-renderer/Color.js
+++ src/01-color-renderer/Color.js
@@ -1,9 +1,13 @@
-export default function Color ({ hex, name }) {
+export default function Color({hex, name}) {
   return (
-    <div
-      className='color-square' style={{ backgroundColor: hex }}
-    >
-      <h2>{name}</h2>
-    </div>
-  )
+    <>
+          <div
+            className="color-square"
+            style={{ backgroundColor: hex }}
+          >
+            <h2>{name}</h2>
+          </div>
+  
+    </>
+  );
 }
