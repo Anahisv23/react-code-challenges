bf37d0dcce2e33b1d9ca0679ffb419ee7ec5140f
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-05-19 15:17:40 -0700

    dog pic react challenge done

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,32 @@
-export default function DogPics () {
+import React, { useState, useEffect } from "react";
+
+export default function DogPics() {
+  const [dogImage, setDogImage] = useState("");
   // API: https://dog.ceo/dog-api/
+
+  
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const doggie = await response.json();
+        return doggie.message
+      } catch (error) {
+        console.log("Could not fetch image");
+      }
+    };
+
+    useEffect(() => {
+     fetchDogImage().then(image => setDogImage(image))
+    }, [])
+
+  const handleClick = async (e) => {
+   setDogImage(await fetchDogImage())
+  };
+
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      <img src={dogImage} />
+      <button onClick={handleClick}>🐶</button>
     </div>
-  )
+  );
 }
